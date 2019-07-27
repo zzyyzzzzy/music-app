d55bd68c5d616c4bed2dd3344c8860f4f5f28319
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2019-07-27 15:12:03 -0700

    bug  fixing

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ const app = () => {
   const play = document.querySelector(".play");
   const outline = document.querySelector(".moving-outline circle");
   const video = document.querySelector(".vid-container video");
+  video.loop = true;
 
   //sounds
-  const sounds = document.querySelector(".sound-picker button");
+  const sounds = document.querySelectorAll(".sound-picker button");
   //Time display
   const timeDisplay = document.querySelector(".time-display");
   const timeSelect = document.querySelectorAll(".time-select button");
@@ -17,6 +18,15 @@ const app = () => {
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  //pick diffsounds
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+
   //play sound
   play.addEventListener("click", () => {
     checkPlaying(song);
@@ -25,6 +35,7 @@ const app = () => {
   //sound
   timeSelect.forEach(option => {
     option.addEventListener("click", function() {
+      song.currentTime = 0;
       fakeDuration = this.getAttribute("data-time");
       timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
         fakeDuration % 60
